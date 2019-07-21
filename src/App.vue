a0b49d4314a7860bca1f62c1c3a1d13ec7d42e38
이광기 <jayden@chequer.io>
2019-07-21 17:30:56 +0900

    Vue 프로젝트 생성

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+   {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
