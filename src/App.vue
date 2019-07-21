17e6badd8fcb11b30bf85017269d09026ec212c7
Jayden <jayden@chequer.io>
2019-07-21 18:26:41 +0900

    Vue 라우터 추가

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-   {{msg}}
+    <Navbar />
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Navbar from './components/Navbar.vue'
 export default {
   name: 'app',
+  components: { Navbar },
   data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
   }
 }
 </script>
---
+++ src/components/NotFound.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    Not Found
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
+
+
