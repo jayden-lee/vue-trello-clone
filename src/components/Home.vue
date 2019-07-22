df8c171db1fde5c612a3bb56f035dafa4ad5b171
Jayden <jayden@chequer.io>
2019-07-22 18:33:22 +0900

    axios를 이용한 백엔드 API 연결

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,6 +3,11 @@
     Home
     <div>
       Board List:
+      <div v-if="loading">Loading...</div>
+      <div v-else>
+        <div v-for="b in boards" :key="b.id">
+          {{b}}
+        </div>
       <ul>
         <li>
           <router-link to="/b/1">Board 1</router-link>
@@ -22,10 +27,36 @@
 </template>
 
 <script>
-export default {};
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      loading: false,
+      boards: [],
+    }
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData() {
+      this.loading = true
+
+      axios.get('http://localhost:3000/boards')
+        .then(res => {
+          this.boards = res.data
+        })
+        .catch(res => {
+          this.$router.replace('/login')
+        })
+        .finally(() => {
+          this.loading = false
+        })
+    }
+  }
+};
 </script>
 
 <style>
 </style>
-
-
